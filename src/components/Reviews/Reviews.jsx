ed39b0bb5790741f4d303b587194f01d398d6162
YuriyShevchyk <97519385+YuriyShevchyk@users.noreply.github.com>
2023-01-04 20:35:44 +0200

    bugfixes

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,23 +1,23 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { getMovieReviews } from 'API/API';
-import ReiwersList from './ReviewsList/ReviewsList';
+import ReviewsList from './ReviewsList/ReviewsList';
 
-export default function Reiwers() {
-  const [movieReiwers, setMovieReiwers] = useState([]);
+export default function Reviews() {
+  const [movieReviews, setMovieReviews] = useState([]);
   const { movieId } = useParams();
 
   useEffect(() => {
-    getMovieReviews(movieId).then(setMovieReiwers);
+    getMovieReviews(movieId).then(setMovieReviews);
   }, [movieId]);
 
-  if (!movieReiwers) {
+  if (!movieReviews) {
     return;
   }
 
-  if (movieReiwers.length === 0) {
+  if (movieReviews.length === 0) {
     return <p>We don't have any reviews for this movie.</p>;
   }
 
-  return <ReiwersList movieReiwers={movieReiwers} />;
+  return <ReviewsList movieReviews={movieReviews} />;
 }
